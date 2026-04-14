39c9aaecbc7281cec116af2c724026273a1c40ee
tanczos-better-prodigy <murphyn@roverkids.org>
2026-04-14 13:45:23 -0400

    completed 1.3

--- Prototype1/Prototype1/Assets/Scripts/FollowPlayer.cs
+++ Prototype1/Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,10 +13,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
+        transform.position = player.transform.position + offset;
     }
 }
